92d473832ef116fe4e444b7f20fe786f7f6f9e1c
sanji118 <sanjishifa32@gmail.com>
2025-06-19 21:56:20 +0600

    added privateprovider

--- src/providers/PrivateRouter.jsx
+++ src/providers/PrivateRouter.jsx
@@ -1,9 +1,16 @@
+import { Navigate } from "react-router-dom";
+import useAuth from "../utils/useAuth"
 
 
-const PrivateRouter = () => {
-  return (
-    <div>PrivateRouter</div>
-  )
+const PrivateRouter = ({children}) => {
+    const {user, loading} = useAuth();
+
+    if(loading){return <span className="loading loading-spinner loading-md"></span>}
+
+    if(!user){
+        return <Navigate to={'/auth/login'} />
+    }
+  return children;
 }
 
 export default PrivateRouter
