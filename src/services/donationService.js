b935ee001cb050511a5fdab710cc8d87c1d8e69a
sanji118 <sanjishifa32@gmail.com>
2025-07-23 02:36:43 +0600

    added crud operations for donation and faqs

--- src/services/donationService.js
+++ src/services/donationService.js
@@ -1,16 +1,31 @@
-import axiosInstance from "../hooks/axiosInstance"
+import axiosInstance from "../hooks/axiosInstance";
 
-export const getDonations = async () =>{
-    const res = await axiosInstance.get('/donations');
-    return res.data;
-}
+export const getDonations = async () => {
+  const res = await axiosInstance.get("/donations");
+  return res.data;
+};
 
 export const getDonationById = async (id) => {
-    const res = await axiosInstance.get(`/donations/id/${id}`);
-    return res.data;
-}
+  const res = await axiosInstance.get(`/donations/${id}`);
+  return res.data;
+};
 
-export const getDonationBySlug = async (slug) => {
-    const res = await axiosInstance.get(`/donations/slug/${slug}`);
-    return res.data;
-}
+export const getDonationsBySlug = async (slug) => {
+  const res = await axiosInstance.get(`/donations/slug/${slug}`);
+  return res.data;
+};
+
+export const createDonation = async (data) => {
+  const res = await axiosInstance.post("/donations", data);
+  return res.data;
+};
+
+export const updateDonation = async (id, data) => {
+  const res = await axiosInstance.patch(`/donations/${id}`, data);
+  return res.data;
+};
+
+export const deleteDonation = async (id) => {
+  const res = await axiosInstance.delete(`/donations/${id}`);
+  return res.data;
+};
--- src/services/faqService.js
+++ src/services/faqService.js
@@ -1,9 +1,30 @@
-import axiosInstance from "../hooks/axiosInstance"
+import axiosInstance from "../hooks/axiosInstance";
 
-export const getFAQs = async () => {
-    const result = await axiosInstance.get('/faq');
-    return result.data;
-}
+export const getFaqs = async () => {
+  const res = await axiosInstance.get("/faqs");
+  return res.data;
+};
+
+export const getFaqById = async (id) => {
+  const res = await axiosInstance.get(`/faqs/${id}`);
+  return res.data;
+};
+
+export const createFaq = async (data) => {
+  const res = await axiosInstance.post("/faqs", data);
+  return res.data;
+};
+
+export const updateFaq = async (id, data) => {
+  const res = await axiosInstance.patch(`/faqs/${id}`, data);
+  return res.data;
+};
+
+export const deleteFaq = async (id) => {
+  const res = await axiosInstance.delete(`/faqs/${id}`);
+  return res.data;
+};
+
 
 export const getFAQsBySlug = async (slug) => {
     const res = await axiosInstance.get(`/faq/slug/${slug}`);
