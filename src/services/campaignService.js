7f844c2dc3b24021f57ec81e7331a7382bedeaa5
sanji118 <sanjishifa32@gmail.com>
2025-07-23 02:35:22 +0600

    added crud operations for campaigns

--- src/services/campaignService.js
+++ src/services/campaignService.js
@@ -1,19 +1,36 @@
-import axiosInstance from "../hooks/axiosInstance"
+import axiosInstance from "../hooks/axiosInstance";
 
-export const getCampaigns = async () =>{
-    const res = await axiosInstance.get('/campaigns');
-    return res.data;
-}
+export const getCampaigns = async () => {
+  const res = await axiosInstance.get("/campaigns");
+  return res.data;
+};
 
 export const getCampaignById = async (id) => {
-    const res = await axiosInstance.get(`/campaigns/id/${id}`);
-    return res.data;
-}
+  const res = await axiosInstance.get(`/campaigns/${id}`);
+  return res.data;
+};
 
 export const getCampaignBySlug = async (slug) => {
-    const res = await axiosInstance.get(`/campaigns/slug/${slug}`);
-    return res.data;
-}
+  const res = await axiosInstance.get(`/campaigns/slug/${slug}`);
+  return res.data;
+};
+
+export const createCampaign = async (data) => {
+  const res = await axiosInstance.post("/campaigns", data);
+  return res.data;
+};
+
+export const updateCampaign = async (id, data) => {
+  const res = await axiosInstance.patch(`/campaigns/${id}`, data);
+  return res.data;
+};
+
+export const deleteCampaign = async (id) => {
+  const res = await axiosInstance.delete(`/campaigns/${id}`);
+  return res.data;
+};
+
+
 
 export const postCommentToCampaign = async ({id, newComment}) => {
     const res = await axiosInstance.post(`campaigns/${id}/comments`, newComment);
